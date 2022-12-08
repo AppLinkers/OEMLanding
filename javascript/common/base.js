869b529da66c4568542fcc736c81b78289850c0d
rio319-an <78468001+rio319-an@users.noreply.github.com>
2022-12-08 20:36:50 +0900

    file path test

--- javascript/common/base.js
+++ javascript/common/base.js
@@ -6,7 +6,7 @@ async function importPage(targetDiv, page) {
     document.querySelector('#' + targetDiv).innerHTML = await fetchAsText(page);
 }
 
-importPage('header', "../../html/common/header.html");
-importPage('nav', "../../html/common/nav.html");
-importPage('login_div', "../../html/common/login_div.html");
-importPage('footer', "../../html/common/footer.html");
+importPage('header', "../html/common/header.html");
+importPage('nav', "../html/common/nav.html");
+importPage('login_div', "../html/common/login_div.html");
+importPage('footer', "../html/common/footer.html");
