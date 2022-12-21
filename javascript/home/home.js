d4c84073118ce3df0bc1bcdd3264bec9d27e12ff
rio319-an <78468001+rio319-an@users.noreply.github.com>
2022-12-21 15:46:53 +0900

    main + button

--- javascript/home/home.js
+++ javascript/home/home.js
@@ -1,4 +1,5 @@
 const tabItem = document.querySelectorAll(".label-list a");
+const tabLink = document.querySelectorAll(".label-buttons a");
 const tabDiv = document.querySelectorAll(".home-target-div");
 
 tabItem.forEach((tab, idx) => {
@@ -10,8 +11,12 @@ tabItem.forEach((tab, idx) => {
         tabItem.forEach(item => {
             item.classList.remove("active")
         })
+        tabLink.forEach(link => {
+            link.classList.remove("active");
+        })
 
         tabItem[idx].classList.add("active");
+        tabLink[idx].classList.add("active");
         tabDiv[idx].classList.add("active");
 
     })
