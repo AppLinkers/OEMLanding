a82caac96b81c982478622c2dc1a702f96c04d29
rio319-an <78468001+rio319-an@users.noreply.github.com>
2023-01-03 20:04:30 +0900

    password_change page && auth.js fix

--- javascript/common/auth.js
+++ javascript/common/auth.js
@@ -1,12 +1,14 @@
 function check_pw() {
-    if (document.getElementById('pw').value != '' && document.getElementById('pw2').value != '') {
-        if (document.getElementById('pw').value == document.getElementById('pw2').value) {
+    if (document.getElementById('pw').value !== '' && document.getElementById('pw2').value !== '') {
+        if (document.getElementById('pw').value === document.getElementById('pw2').value) {
             document.getElementById('pw-alert').innerHTML = '비밀번호가 일치합니다.'
             document.getElementById('pw-alert').style.color = "var(--color-secondary)";
+            document.getElementById('submit').disabled = false;
         }
         else {
             document.getElementById('pw-alert').innerHTML = '비밀번호가 일치하지 않습니다.';
             document.getElementById('pw-alert').style.color = "var(--color-red)";
+            document.getElementById('submit').disabled = true;
             return;
         }
     }
